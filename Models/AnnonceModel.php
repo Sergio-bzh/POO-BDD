dc1f23401c18df07cbbe1d662de13a3094c01f66
sergio-nunez <sergio.nunez@laposte.net>
2024-09-01 20:17:46 +0200

    Add methods to AnnonceModel class accordingly the annonces's table fields

--- Models/AnnonceModel.php
+++ Models/AnnonceModel.php
@@ -2,10 +2,109 @@
 
 namespace App\Models;
 
+use DateTimeImmutable;
+
 class AnnonceModel extends Model
 {
+    protected int $id;
+    protected string $titre;
+    protected string $description;
+    protected DateTimeImmutable $created_at;
+    protected bool $actif;
+
+    /**
+     * Constructeur d'objets annonce
+     * Les objets instanciés sont stoqués dans la table annonces
+     */
     public function __construct()
     {
-        $this->table = 'annonce';
+        $this->table = 'annonces';
+    }
+
+    /**
+     * Accesseur retournant un entier correspondant l'id de l'objet
+     *
+     * @return integer
+     */
+    public function getId():int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Accesseur retourant une chaîne avec le titre de l'objet
+     *
+     * @return string
+     */
+    public function getTitre():string
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Mutateur qui modifie le titre de l'objet
+     *
+     * @param string $titre Titre de l'objet
+     * @return self
+     */
+    public function setTitre(string $titre):self
+    {
+        $this->titre = $titre;
+        return $this;
+    }
+
+    /**
+     * Accesseur qui retourne une chaîne avec la description de l'objet
+     *
+     * @return string
+     */
+    public function getDescription():string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Mutateur qui modifie la description de l'objet
+     *
+     * @param string $description
+     * @return self
+     */
+    public function setDescription(string $description):self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Accesseur qui retourne la date de création de l'objet
+     *
+     * @return DateTimeImmutable
+     */
+    public function getCreatedAt():DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Accesseur qui retourne le boulean actif de l'objet
+     *
+     * @return boolean
+     */
+    public function getActif():bool
+    {
+        return $this->actif;
+    }
+
+
+    /**
+     * Mutateur permettant de modifier l'état actif de l'objet
+     *
+     * @param boolean $actif
+     * @return self
+     */
+    public function setActif(bool $actif):self
+    {
+        $this->actif = $actif;
+        return $this;
     }
 }
--- index.php
+++ index.php
@@ -6,16 +6,31 @@ use App\Models\AnnonceModel;
 require_once __DIR__ . ('/Autoloader.php');
 Autoloader::register();
 
-echo('<h1>Patrons de conception | Le Singleton</h>');
+// echo('<h1>Patrons de conception | Le Singleton</h>');
 
+// J'instancie un objet de typa AnnonceModel dans la variable $model
 $model = new AnnonceModel;
 
+/*  J'appelle les setters de la classe AnnonceModel et je définis les valeurs 
+    des propriétés de l'objet puis je le stoque dans la variable $annonce */
+$annonce = $model
+    ->setTitre('Nouvelle annoce')
+    ->setDescription('Nouvelle description')
+    ->setActif(1);
+
+/*  J'appelle la méthode create de la classe AnnonceModel et lui passe 
+    l'objet $annonce qui contient les valeurs */
+$model->create($annonce);
+
+
 echo '<pre>';
-    var_dump($model);
+    // var_dump($model);
 
-    var_dump($model->findAll());
+    // var_dump($model->findAll());
 
-    var_dump($model->findBy(['id' => null, 'actif' => null]));
+    // var_dump($model->findBy(['id' => null, 'actif' => null]));
 
-    var_dump($model->find('7'));
-echo '</pre>';
+    // var_dump($model->find(7));
+
+    // var_dump($annonce);
+    echo '</pre>';
